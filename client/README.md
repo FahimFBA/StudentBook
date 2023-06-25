d1599b453dd81f80e4fe40969e01a7678d218db6
FahimFBA <fahimbinamin@gmail.com>
2023-06-25 20:25:17 +0600

    initial data

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# To run
+
+`npm run dev`
---
+++ client/node_modules/rollup/dist/es/rollup.js
@@ -0,0 +1,17 @@
+/*
+  @license
+	Rollup.js v3.25.2
+	Sat, 24 Jun 2023 20:37:46 GMT - commit 91bf079599b84f97ea694dd1ecd0108ecf53fc97
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+export { version as VERSION, defineConfig, rollup, watch } from './shared/node-entry.js';
+import 'node:path';
+import 'path';
+import 'node:process';
+import 'node:perf_hooks';
+import 'node:crypto';
+import 'node:fs/promises';
+import 'tty';
