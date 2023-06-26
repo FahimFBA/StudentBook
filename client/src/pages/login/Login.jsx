b065a9007dfdb0bd585cbec4047313e4f3b4cb51
FahimFBA <fahimbinamin@gmail.com>
2023-06-26 23:25:39 +0600

    react router: button implimentation

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import "./login.scss"
 
 const Login = () => {
@@ -13,7 +14,9 @@ const Login = () => {
                         education and career needs.
                     </p>
                     <span>Don't you have an account?</span>
-                    <button>Register!</button>
+                    <Link to="/register">
+                        <button>Register!</button>
+                    </Link>
                 </div>
                 <div className="right">
                     <h1>Login</h1>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./register.scss"
 
 const Register = () => {
@@ -13,7 +14,9 @@ const Register = () => {
             education and career needs.
           </p>
           <span>Do you have an account?</span>
-          <button>Login!</button>
+          <Link to="/login">
+            <button>Login!</button>
+          </Link>
         </div>
         <div className="right">
           <h1>Register</h1>
