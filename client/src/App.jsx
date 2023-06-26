4dc709d9cf0b5a03f80701131182e0dae5b55ab4
FahimFBA <fahimbinamin@gmail.com>
2023-06-26 23:40:53 +0600

    currentUser: init false

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,51 @@
+import LeftBar from "./components/leftbar/LeftBar";
+import NavBar from "./components/navbar/NavBar";
+import RightBar from "./components/rightbar/RightBar";
 import Login from "./pages/login/Login"
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
 
 function App() {
 
+  const currentUser = false;
+
   const Layout = () => {
     return (
       <div>
-        
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
       </div>
     )
   }
 
   const router = createBrowserRouter([
+
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        },
+      ]
+
+    },
     {
       path: "/login",
       element: <Login />,
