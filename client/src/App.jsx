15fe188225d6b36cea094f26928a4f40ed0ed0f3
FahimFBA <fahimbinamin@gmail.com>
2023-06-26 23:23:35 +0600

    react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
 import Login from "./pages/login/Login"
+import Register from "./pages/register/Register";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+
   return (
     <div>
-      <Login />
+      <RouterProvider router={router} />
     </div >
   );
 }
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -2,8 +2,32 @@ import "./register.scss"
 
 const Register = () => {
   return (
-    <div className="register">Register</div>
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Student
+            Book</h1>
+          <p>
+            The only social media
+            you need for your
+            education and career needs.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login!</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" required />
+            <input type="text" placeholder="Name" required />
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Register
+export default Register;
