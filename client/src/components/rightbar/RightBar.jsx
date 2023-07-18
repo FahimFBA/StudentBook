5c5d543a43cb379eb9e3d64259c4d968af47d975
FahimFBA <fahimbinamin@gmail.com>
2023-07-18 10:55:40 +0600

    Right menu UI design (54:31)

--- client/src/components/rightbar/RightBar.jsx
+++ client/src/components/rightbar/RightBar.jsx
@@ -2,7 +2,33 @@ import "./rightbar.scss"
 
 const RightBar = () => {
     return (
-        <div className="rightBar">RightBar</div>
+        <div className="rightBar">
+            <div className="container">
+                <div className="item">
+                    <span>Suggestions For You</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80" alt="" />
+                            <span>Sofia</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="" />
+                            <span>Diana</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
