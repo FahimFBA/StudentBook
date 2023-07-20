3fa86e1dc1ee77030c50affdc11c494f740d57ab
FahimFBA <fahimbinamin@gmail.com>
2023-07-20 22:54:53 +0600

    feat: handleLogin

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,34 +1,39 @@
-import { Link } from "react-router-dom"
-import "./login.scss"
+import { Link } from "react-router-dom";
+import "./login.scss";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 const Login = () => {
-    return (
-        <div className="login">
-            <div className="card">
-                <div className="left">
-                    <h1>Student
-                        Book</h1>
-                    <p>
-                        The only social media
-                        you need for your
-                        education and career needs.
-                    </p>
-                    <span>Don't you have an account?</span>
-                    <Link to="/register">
-                        <button>Register!</button>
-                    </Link>
-                </div>
-                <div className="right">
-                    <h1>Login</h1>
-                    <form>
-                        <input type="text" placeholder="Username" required />
-                        <input type="password" placeholder="Password" required />
-                        <button>Login</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const { login } = useContext(AuthContext);
 
-export default Login
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Student Book</h1>
+          <p>
+            The only social media you need for your education and career needs.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register!</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" required />
+            <input type="password" placeholder="Password" required />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
