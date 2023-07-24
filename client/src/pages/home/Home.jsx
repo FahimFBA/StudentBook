9f0f4099a635ce2f331e543009cf1ed57cf1d3d9
FahimFBA <fahimbinamin@gmail.com>
2023-07-24 19:12:20 +0600

    stories jsx and scss

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,60 +1,14 @@
-import "./home.scss"
+import "./home.scss";
+import Posts from "../../components/posts/Posts";
+import Stories from "../../components/stories/Stories";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <Stories />
+      <Posts />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
