1c0cf6ed6b2436da24aa3f1691a0c71be01e08d6
FahimFBA <fahimbinamin@gmail.com>
2023-07-27 19:31:39 +0600

    add post sharing feature

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import "./home.scss";
 import Posts from "../../components/posts/Posts";
 import Stories from "../../components/stories/Stories";
+import Share from "../../components/share/Share";
 
 const Home = () => {
   return (
     <div className="home">
       <Stories />
+      <Share />
       <Posts />
     </div>
   );
