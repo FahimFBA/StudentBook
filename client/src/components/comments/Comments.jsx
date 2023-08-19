18c4ca0d1f29a4facfc42f075741841cf6a6bd2d
FahimFBA <fahimbinamin@gmail.com>
2023-08-19 15:15:22 +0600

    remove: temporary comment data

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -4,25 +4,7 @@ import { AuthContext } from "../../context/authContext";
 
 const Comments = () => {
   const { currentUser } = useContext(AuthContext);
-  //Temporary
-  const comments = [
-    {
-      id: 1,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Fahim Amin",
-      userId: 1,
-      profilePicture:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      id: 2,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Fahim Amin",
-      userId: 2,
-      profilePicture:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-  ];
+  
   return (
     <div className="comments">
       <div className="write">
