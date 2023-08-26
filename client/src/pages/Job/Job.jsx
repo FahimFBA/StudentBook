6adcc1618250dfeeac46cf94d1e15e2fe138083a
FahimFBA <fahimbinamin@gmail.com>
2023-08-26 19:17:44 +0600

    feat: input box for job salary

--- client/src/pages/Job/Job.jsx
+++ client/src/pages/Job/Job.jsx
@@ -161,6 +161,22 @@ const Job = () => {
           />
         </div>
         <div>
+          {/* Job Salary */}
+          <label
+            for="job_salary"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Salary/Salary Range
+          </label>
+          <input
+            type="text"
+            id="job_salary"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter your salary range..."
+            required={true}
+          />
+        </div>
+        <div>
           <label
             for="company"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
