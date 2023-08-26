b67c94839a2d3921b7d7b114fc8f13a06dc5a5b6
FahimFBA <fahimbinamin@gmail.com>
2023-08-26 19:15:22 +0600

    feat: input box for job requirement

--- client/src/pages/Job/Job.jsx
+++ client/src/pages/Job/Job.jsx
@@ -144,6 +144,22 @@ const Job = () => {
             required={true}
           />
         </div>
+        <div class="mb-6">
+          {/* Job Requirement */}
+          <label
+            for="job_requirement"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Job Requirement
+          </label>
+          <input
+            type="text"
+            id="job_requirement"
+            class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write down the complete job requirement"
+            required={true}
+          />
+        </div>
         <div>
           <label
             for="company"
