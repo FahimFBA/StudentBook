0f5e4cbedd45dfb14a22328c51378425263aaff8
FahimFBA <fahimbinamin@gmail.com>
2023-08-26 19:23:18 +0600

    feat: input box for linkedin and facebook page

--- client/src/pages/Job/Job.jsx
+++ client/src/pages/Job/Job.jsx
@@ -112,6 +112,38 @@ const Job = () => {
             required={true}
           />
         </div>
+        <div>
+          {/* Job Provider Company Twitter */}
+          <label
+            for="job_provider_company_twitter"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Official Twitter/X
+          </label>
+          <input
+            type="url"
+            id="job_provider_company_twitter"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="https://twitter.com/BrainStation23"
+            required={true}
+          />
+        </div>
+        <div>
+          {/* Job Provider Company Facebook */}
+          <label
+            for="job_provider_company_facebook"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Official Facebook Page
+          </label>
+          <input
+            type="url"
+            id="job_provider_company_facebook"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="https://www.facebook.com/brainstation23"
+            required={true}
+          />
+        </div>
         {/* Job Provider Company Email */}
         <div class="mb-6">
           <label
