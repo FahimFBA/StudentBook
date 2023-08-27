4da69ec860cc45115e70d91f4b4fd58f1c77d13b
FahimFBA <fahimbinamin@gmail.com>
2023-08-27 09:31:44 +0600

    schema

--- schema/studentbookdb.sql
+++ schema/studentbookdb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `studentbookdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `studentbookdb`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: studentbookdb
@@ -421,4 +423,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-27  9:21:31
+-- Dump completed on 2023-08-27  1:07:57
