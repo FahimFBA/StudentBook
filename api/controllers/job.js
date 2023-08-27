78d50dcff924320bc121e2ee2ea2a1e148c7dba9
FahimFBA <fahimbinamin@gmail.com>
2023-08-27 09:38:24 +0600

    api: delete a job and refetch all job

--- api/controllers/job.js
+++ api/controllers/job.js
@@ -89,3 +89,21 @@ export const getAllJobs = (req, res) => {
 
     });
 };
+
+
+export const deleteOneJob = (req, res) => {
+    
+        const token = req.cookies.accessToken;
+        if (!token) return res.status(401).json("Not logged in!");
+    
+        jwt.verify(token, "secretkey", (err, userInfo) => {
+            if (err) return res.status(403).json("Token is not valid!");
+    
+            const q = "DELETE FROM jobtable WHERE job_id = ?";
+    
+            db.query(q, [req.params.id], (err, data) => {
+                if (err) return res.status(500).json(err);
+                return res.status(200).json("Job has been deleted!");
+            });
+        });
+}
--- api/routes/jobs.js
+++ api/routes/jobs.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createJob, getAllJobs } from '../controllers/job.js';
+import { createJob, getAllJobs, deleteOneJob } from '../controllers/job.js';
 
 const router = express.Router();
 
 router.get("/get-all-jobs", getAllJobs);
 router.post("/", createJob);
+router.delete("/:id", deleteOneJob);
 
 export default router;
