ca2f8bee8b03e918c89888befd93f7ec5dbf198e
FahimFBA <fahimbinamin@gmail.com>
2023-08-27 09:22:17 +0600

    schema

--- schema/studentbookdb.sql
+++ schema/studentbookdb.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `studentbookdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `studentbookdb`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: studentbookdb
@@ -32,8 +30,9 @@ CREATE TABLE `articletable` (
   `article_title` varchar(45) NOT NULL,
   PRIMARY KEY (`article_id`),
   UNIQUE KEY `article_id_UNIQUE` (`article_id`),
-  KEY `user_id_idx` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `article_user_fk` FOREIGN KEY (`user_id`) REFERENCES `usertable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +64,7 @@ CREATE TABLE `commentstable` (
   KEY `comment_user_id_idx` (`user_id`),
   CONSTRAINT `comment_user_id` FOREIGN KEY (`user_id`) REFERENCES `usertable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES `poststable` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +73,7 @@ CREATE TABLE `commentstable` (
 
 LOCK TABLES `commentstable` WRITE;
 /*!40000 ALTER TABLE `commentstable` DISABLE KEYS */;
-INSERT INTO `commentstable` VALUES (1,'new comment from Joy',NULL,8,34);
+INSERT INTO `commentstable` VALUES (1,'new comment from Joy',NULL,8,34),(10,'I am commenting with Arifeen','2023-08-27 00:48:12',8,96);
 /*!40000 ALTER TABLE `commentstable` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -214,7 +213,7 @@ CREATE TABLE `likestable` (
   KEY `likePostId_idx` (`post_id`),
   CONSTRAINT `likeeUserId` FOREIGN KEY (`user_id`) REFERENCES `usertable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `likePostId` FOREIGN KEY (`post_id`) REFERENCES `poststable` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,7 +222,7 @@ CREATE TABLE `likestable` (
 
 LOCK TABLES `likestable` WRITE;
 /*!40000 ALTER TABLE `likestable` DISABLE KEYS */;
-INSERT INTO `likestable` VALUES (10,8,36),(11,8,34);
+INSERT INTO `likestable` VALUES (10,8,36),(11,8,34),(17,8,94);
 /*!40000 ALTER TABLE `likestable` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -244,7 +243,7 @@ CREATE TABLE `poststable` (
   UNIQUE KEY `post_id_UNIQUE` (`post_id`),
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `usertable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=97 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -253,7 +252,7 @@ CREATE TABLE `poststable` (
 
 LOCK TABLES `poststable` WRITE;
 /*!40000 ALTER TABLE `poststable` DISABLE KEYS */;
-INSERT INTO `poststable` VALUES (1,'this is a post',NULL,3,NULL),(2,'this is another post',NULL,6,NULL),(3,'from user 7',NULL,7,NULL),(4,'asdasd',NULL,6,NULL),(5,'asdasdas',NULL,7,NULL),(6,'jane theke',NULL,8,NULL),(7,'from postman',NULL,NULL,'2023-08-16 23:40:20'),(8,'from postman 2',NULL,NULL,'2023-08-16 23:41:47'),(9,'from postman 232',NULL,NULL,'2023-08-16 23:44:22'),(10,'from postman (testing for showing the user id automatically)',NULL,8,'2023-08-16 23:46:16'),(11,'asdasd',NULL,8,'2023-08-17 17:31:48'),(12,'This is from Fahim',NULL,8,'2023-08-17 17:31:58'),(13,'desc with an image',NULL,8,'2023-08-17 17:53:14'),(14,'desc with an image',NULL,8,'2023-08-17 17:53:43'),(15,'',NULL,8,'2023-08-17 17:55:19'),(16,'img',NULL,8,'2023-08-17 17:56:01'),(17,'asdasd',NULL,8,'2023-08-17 17:58:33'),(18,'from postman (testing for showing the user id automatically)',NULL,8,'2023-08-17 22:55:02'),(19,'from postman (testing for showing the user id automatically)asdasd',NULL,8,'2023-08-17 23:21:52'),(20,'posting',NULL,8,'2023-08-17 23:36:09'),(21,'postingasdas',NULL,8,'2023-08-17 23:46:09'),(22,'I am with Joy Bhai now',NULL,8,'2023-08-18 00:21:28'),(23,'asdasdasdawt3wt23qerq','',8,'2023-08-18 00:26:28'),(24,'asdasdasdawt3wt23qerq','1692296795439istockphoto-1322992439-612x612.jpg',8,'2023-08-18 00:26:35'),(25,'Again trying','1692297003778big_news1.gif',8,'2023-08-18 00:30:03'),(26,'Again trying without image','1692297008684big_news1.gif',8,'2023-08-18 00:30:08'),(27,'asdasdas','',8,'2023-08-18 00:30:19'),(28,'asdasdas',NULL,8,'2023-08-18 00:31:14'),(29,'3ewqy3gesf',NULL,8,'2023-08-18 00:31:23'),(30,'asfgawfgasd',NULL,8,'2023-08-18 00:32:22'),(31,'',NULL,8,'2023-08-18 00:32:30'),(32,'asdasdasde3tasfsdf','',8,'2023-08-18 00:33:47'),(33,'','1692297230592istockphoto-1322992439-612x612.jpg',8,'2023-08-18 00:33:50'),(34,'asdatgfwa','1692297234865istockphoto-1322992439-612x612.jpg',8,'2023-08-18 00:33:54'),(35,'post number 35. Posting without image ','',8,'2023-08-18 10:56:26'),(36,'post number 35. Posting with image ','1692334610806Introduction To Open Source ( à¦à¦ªà§à¦¨ à¦¸à§à¦°à§à¦¸ à¦¨à¦¿à§à§ à¦ªà¦°à¦¿à¦à¦¿à¦¤à¦¿ à¦®à§à¦²à¦ à¦à¦²à§à¦à¦¨à¦¾ ).png',8,'2023-08-18 10:56:50'),(38,'another user\'s post',NULL,9,NULL),(40,'test','',8,'2023-08-21 18:15:27'),(42,'Fahim again','',8,'2023-08-21 18:24:22'),(43,'post with image','1692892118822scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 21:48:38'),(44,'creating new post',NULL,8,'2023-08-24 22:36:04'),(45,'creating new post',NULL,8,'2023-08-24 22:37:14'),(46,'testing unwrap','',8,'2023-08-24 23:01:25'),(47,'testing unwrap','1692896495779scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:01:35'),(48,'testing unwrap 2','',8,'2023-08-24 23:01:53'),(49,'testing unwrap 3','',8,'2023-08-24 23:04:09'),(50,'testing unwrap 3 ','1692896659832scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:04:19'),(53,'','1692896751582scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:05:51'),(54,'','1692896759789scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:05:59'),(57,'','1692896878486scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:07:58'),(58,'asdasd','',8,'2023-08-24 23:08:01'),(59,'asdfasd','',8,'2023-08-24 23:08:09'),(60,'asdasd','',8,'2023-08-24 23:09:06'),(61,'','1692896949728scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:09:09'),(62,'asdasd','',8,'2023-08-24 23:09:16'),(63,'asdasd','1692896999710scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:09:59'),(64,'asdasd','',8,'2023-08-24 23:10:02'),(66,'','',8,'2023-08-24 23:10:22'),(67,'','1692897027154scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:10:27'),(68,'asdas','',8,'2023-08-24 23:10:48'),(69,'asdasd','1692897053608scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:10:53'),(70,'','',8,'2023-08-24 23:10:56'),(71,'asdasd','',8,'2023-08-24 23:11:03'),(72,'asdasdas','',8,'2023-08-24 23:11:28'),(73,'','1692897092408scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:11:32'),(74,'asdasd','',8,'2023-08-24 23:11:36'),(75,'','',8,'2023-08-24 23:11:41'),(76,'asdasdasd','',8,'2023-08-24 23:12:18'),(77,'','1692897142652scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:12:22'),(78,'asdasdasd','',8,'2023-08-24 23:12:30'),(79,'','',8,'2023-08-24 23:12:37'),(80,'','',8,'2023-08-24 23:12:42'),(81,'','1692897290071scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:14:50'),(82,'asdasd','',8,'2023-08-24 23:14:52'),(83,'asdasd','',8,'2023-08-24 23:14:55'),(84,'','',8,'2023-08-24 23:14:58'),(85,'asdasd','',8,'2023-08-24 23:17:00'),(86,'','1692897442200scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:17:22'),(87,'','1692897478807scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:17:58'),(88,'','',8,'2023-08-24 23:23:04'),(89,'','',8,'2023-08-24 23:23:17'),(90,'asdasd','',8,'2023-08-24 23:24:42'),(91,'','',8,'2023-08-24 23:25:42'),(92,'','',8,'2023-08-24 23:25:50'),(93,'','1692898044713scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:27:24'),(94,'','',8,'2023-08-24 23:27:29');
+INSERT INTO `poststable` VALUES (1,'this is a post',NULL,3,NULL),(2,'this is another post',NULL,6,NULL),(3,'from user 7',NULL,7,NULL),(4,'asdasd',NULL,6,NULL),(5,'asdasdas',NULL,7,NULL),(6,'jane theke',NULL,8,NULL),(7,'from postman',NULL,NULL,'2023-08-16 23:40:20'),(8,'from postman 2',NULL,NULL,'2023-08-16 23:41:47'),(9,'from postman 232',NULL,NULL,'2023-08-16 23:44:22'),(10,'from postman (testing for showing the user id automatically)',NULL,8,'2023-08-16 23:46:16'),(11,'asdasd',NULL,8,'2023-08-17 17:31:48'),(12,'This is from Fahim',NULL,8,'2023-08-17 17:31:58'),(13,'desc with an image',NULL,8,'2023-08-17 17:53:14'),(14,'desc with an image',NULL,8,'2023-08-17 17:53:43'),(15,'',NULL,8,'2023-08-17 17:55:19'),(16,'img',NULL,8,'2023-08-17 17:56:01'),(17,'asdasd',NULL,8,'2023-08-17 17:58:33'),(18,'from postman (testing for showing the user id automatically)',NULL,8,'2023-08-17 22:55:02'),(19,'from postman (testing for showing the user id automatically)asdasd',NULL,8,'2023-08-17 23:21:52'),(20,'posting',NULL,8,'2023-08-17 23:36:09'),(21,'postingasdas',NULL,8,'2023-08-17 23:46:09'),(22,'I am with Joy Bhai now',NULL,8,'2023-08-18 00:21:28'),(23,'asdasdasdawt3wt23qerq','',8,'2023-08-18 00:26:28'),(24,'asdasdasdawt3wt23qerq','1692296795439istockphoto-1322992439-612x612.jpg',8,'2023-08-18 00:26:35'),(25,'Again trying','1692297003778big_news1.gif',8,'2023-08-18 00:30:03'),(26,'Again trying without image','1692297008684big_news1.gif',8,'2023-08-18 00:30:08'),(27,'asdasdas','',8,'2023-08-18 00:30:19'),(28,'asdasdas',NULL,8,'2023-08-18 00:31:14'),(29,'3ewqy3gesf',NULL,8,'2023-08-18 00:31:23'),(30,'asfgawfgasd',NULL,8,'2023-08-18 00:32:22'),(31,'',NULL,8,'2023-08-18 00:32:30'),(32,'asdasdasde3tasfsdf','',8,'2023-08-18 00:33:47'),(33,'','1692297230592istockphoto-1322992439-612x612.jpg',8,'2023-08-18 00:33:50'),(34,'asdatgfwa','1692297234865istockphoto-1322992439-612x612.jpg',8,'2023-08-18 00:33:54'),(35,'post number 35. Posting without image ','',8,'2023-08-18 10:56:26'),(36,'post number 35. Posting with image ','1692334610806Introduction To Open Source ( à¦à¦ªà§à¦¨ à¦¸à§à¦°à§à¦¸ à¦¨à¦¿à§à§ à¦ªà¦°à¦¿à¦à¦¿à¦¤à¦¿ à¦®à§à¦²à¦ à¦à¦²à§à¦à¦¨à¦¾ ).png',8,'2023-08-18 10:56:50'),(38,'another user\'s post',NULL,9,NULL),(40,'test','',8,'2023-08-21 18:15:27'),(42,'Fahim again','',8,'2023-08-21 18:24:22'),(43,'post with image','1692892118822scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 21:48:38'),(44,'creating new post',NULL,8,'2023-08-24 22:36:04'),(45,'creating new post',NULL,8,'2023-08-24 22:37:14'),(46,'testing unwrap','',8,'2023-08-24 23:01:25'),(47,'testing unwrap','1692896495779scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:01:35'),(48,'testing unwrap 2','',8,'2023-08-24 23:01:53'),(49,'testing unwrap 3','',8,'2023-08-24 23:04:09'),(50,'testing unwrap 3 ','1692896659832scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:04:19'),(53,'','1692896751582scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:05:51'),(54,'','1692896759789scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:05:59'),(57,'','1692896878486scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:07:58'),(58,'asdasd','',8,'2023-08-24 23:08:01'),(59,'asdfasd','',8,'2023-08-24 23:08:09'),(60,'asdasd','',8,'2023-08-24 23:09:06'),(61,'','1692896949728scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:09:09'),(62,'asdasd','',8,'2023-08-24 23:09:16'),(63,'asdasd','1692896999710scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:09:59'),(64,'asdasd','',8,'2023-08-24 23:10:02'),(66,'','',8,'2023-08-24 23:10:22'),(67,'','1692897027154scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:10:27'),(68,'asdas','',8,'2023-08-24 23:10:48'),(69,'asdasd','1692897053608scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:10:53'),(70,'','',8,'2023-08-24 23:10:56'),(71,'asdasd','',8,'2023-08-24 23:11:03'),(72,'asdasdas','',8,'2023-08-24 23:11:28'),(73,'','1692897092408scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:11:32'),(74,'asdasd','',8,'2023-08-24 23:11:36'),(75,'','',8,'2023-08-24 23:11:41'),(76,'asdasdasd','',8,'2023-08-24 23:12:18'),(77,'','1692897142652scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:12:22'),(78,'asdasdasd','',8,'2023-08-24 23:12:30'),(79,'','',8,'2023-08-24 23:12:37'),(80,'','',8,'2023-08-24 23:12:42'),(81,'','1692897290071scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:14:50'),(82,'asdasd','',8,'2023-08-24 23:14:52'),(83,'asdasd','',8,'2023-08-24 23:14:55'),(84,'','',8,'2023-08-24 23:14:58'),(85,'asdasd','',8,'2023-08-24 23:17:00'),(86,'','1692897442200scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:17:22'),(87,'','1692897478807scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:17:58'),(88,'','',8,'2023-08-24 23:23:04'),(89,'','',8,'2023-08-24 23:23:17'),(90,'asdasd','',8,'2023-08-24 23:24:42'),(91,'','',8,'2023-08-24 23:25:42'),(92,'','',8,'2023-08-24 23:25:50'),(93,'','1692898044713scenic-aesthetic-1920x1080-12513.jpg',8,'2023-08-24 23:27:24'),(94,'','',8,'2023-08-24 23:27:29'),(95,'','1693075661093Screenshot 2023-08-27 004735.png',8,'2023-08-27 00:47:41'),(96,'','1693075670196Screenshot 2023-08-27 004735.png',8,'2023-08-27 00:47:50');
 /*!40000 ALTER TABLE `poststable` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -422,4 +421,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-26 23:57:27
+-- Dump completed on 2023-08-27  9:21:31
