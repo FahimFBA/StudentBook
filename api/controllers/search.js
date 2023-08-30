6db077047a8022a00ac20752df035de02d0affe2
FahimFBA <fahimbinamin@gmail.com>
2023-08-30 21:40:36 +0600

    Update search.js

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -2,7 +2,6 @@ import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
 import moment from "moment";
 
-// my job is to find the user by user_name from my usertable database table
 export const searchByUserName = (req, res) => {
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json("Not logged in!");
@@ -10,9 +9,7 @@ export const searchByUserName = (req, res) => {
     jwt.verify(token, "secretkey", (err, userInfo) => {
         if (err) return res.status(403).json("Token is not valid!");
 
-        // my job is to get the title from various datat table
 
-        // const q = "SELECT * FROM usertable WHERE user_fullname LIKE ?";
 
         const q = `SELECT ut.*, st.student_cgpa
         FROM usertable ut
